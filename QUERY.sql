01aebca3d43d5a1cac78fcf908f477b63bb56a99
Tamim Khan <md.tamim.iq.khan@gmail.com>
2026-06-15 16:24:04 +0600

    Added Query Retrieving match booking details along with the User's full name and the scheduled Match fixture teams

--- QUERY.sql
+++ QUERY.sql
@@ -12,3 +12,9 @@ WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
 SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') as "systematic_status"
 FROM Bookings 
 WHERE payment_status IS NULL;
+
+
+-- Query 4
+SELECT booking_id, full_name, fixture, total_cost FROM Bookings b
+INNER JOIN Matches m ON b.match_id = m.match_id
+INNER JOIN Users u ON b.user_id = u.user_id;
