14c03814e0b7729b5ceac08fdbeddbb1985f6faa
Tamim Khan <md.tamim.iq.khan@gmail.com>
2026-06-15 14:41:52 +0600

    Added Query Retrieving all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'

--- QUERY.sql
+++ QUERY.sql
@@ -6,3 +6,9 @@ WHERE tournament_category = 'Champions League' AND match_status = 'Available';
 -- Query 2
 SELECT user_id, full_name, email FROM Users
 WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
+
+
+-- Query 3
+SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') as "systematic_status"
+FROM Bookings 
+WHERE payment_status IS NULL;
